1c15889eb5c2452d30d21fac25f62764f70b1a21
daniel sc <su7cheng@gmail.com>
2018-10-01 16:18:43 -0300

    Create routes to new, edit and delete book.

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-	bookstore = session.query(Bookstore).first()
+@app.route('/bookstores/<int:bookstore_id>/')
+def bookstoreCatalog(bookstore_id):
+	bookstore = session.query(Bookstore).filter_by(id=bookstore_id).one()
 	books = session.query(Book).filter_by(bookstore_id=bookstore.id)
 	output = ''
 	for i in books:
@@ -30,6 +30,19 @@ def HelloWorld():
 		output += '</br>'
 		output += '</br>'
 	return output
+
+@app.route('/bookstore/<int:bookstore_id>/new/')
+def newBook(bookstore_id):
+	return "page to create a new book."
+
+@app.route('/bookstore/<int:bookstore_id>/<int:book_id>/edit/')
+def editBookDetails(bookstore_id, book_id):
+	return "page to edit a book details."
+
+@app.route('/bookstore/<int:bookstore_id>/<int:book_id>/delete/')
+def deleteBook(bookstore_id, book_id):
+	return "page to delete a book."
+
 
 if __name__ == '__main__':
 	app.debug = True
