9ae20b71eecffcf55b8f85de3c51c43e05231dd5
daniel sc <su7cheng@gmail.com>
2018-10-05 10:52:22 -0300

    Add createUser function.

--- app.py
+++ app.py
@@ -33,6 +33,18 @@ def close_session(session):
     session.close()
 
 
+def createUser(login_session):
+    open_session(engine)
+    newUser = User(name = login_session['username'],
+                   email = login_session['email'],
+                   picture = login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    close_session()
+    return user.id
+
+
 @app.route('/login')
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
