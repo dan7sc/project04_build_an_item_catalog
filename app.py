2ad4a9861a4ed19489035aa4bd5269240f0c9194
daniel sc <su7cheng@gmail.com>
2018-10-07 20:55:20 -0300

    Add disconnect function.

--- app.py
+++ app.py
@@ -248,6 +248,28 @@ def fbdisconnect():
     return "You have been logged out."
 
 
+@app.route('/disconnect')
+def disconnect():
+    if 'provider' in login_session:
+        if login_session['provider'] == 'google':
+            gdisconnect()
+            del login_session['gplus_id']
+            del login_session['credentials']
+        if login_session['provider'] == 'facebook':
+            fbdisconnect()
+            del login_session['facebook_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        del login_session['user_id']
+        del login_session['provider']
+        flash("You have successfully been logged out.")
+        return redirect(url_for('showBookstores'))
+    else:
+        flash("You were not logged in.")
+        return redirect(url_for('showBookstores'))
+
+
 @app.route('/')
 @app.route('/bookstores/')
 def showBookstores():
