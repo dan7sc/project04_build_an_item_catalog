1f6f1d0ccea6472d2a5884e163eb010a94958d0c
daniel sc <su7cheng@gmail.com>
2018-10-28 11:29:46 -0300

    Add json files.

--- lotsofbooks.py
+++ lotsofbooks.py
@@ -1,157 +1,67 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import Bookstore, Base, Book, User
+from database_setup import (
+    Bookstore, Base, Book, User
+    )
 
+import json
 
 engine = create_engine('sqlite:///virtualbookstores.db')
 
 Base.metadata.bind = engine
-
 DBSession = sessionmaker(bind=engine)
-
 session = DBSession()
 
-user1 = User(name="Daniel Santiago",
-			 email="dansan@fake.com",
-			 picture="")
-session.add(user1)
-session.commit()
 
-bookstore1 = Bookstore(user_id=1,
-				   name="Urban Fantasy")
-session.add(bookstore1)
-session.commit()
-
-book1 = Book(title="The Hobbit",
-			 author="J.R.R. Tolkien",
-			 genre="Fantasy",
-			 description="A book about fantasy.",
-			 price="$11.99",
-			 bookstore=bookstore1)
-session.add(book1)
-session.commit()
-
-book2 = Book(title="The Fellowship of the Ring",
-			 author="J.R.R. Tolkien",
-			 genre="Fantasy",
-			 description="A book about fantasy.",
-			 price="$13.99",
-			 bookstore=bookstore1)
-session.add(book2)
-session.commit()
-
-book3 = Book(title="The Two Towers",
-			 author="J.R.R. Tolkien",
-			 genre="Fantasy",
-			 description="A book about fantasy.",
-			 price="$12.99",
-			 bookstore=bookstore1)
-session.add(book3)
-session.commit()
-
-book4 = Book(title="The Return of the King",
-			 author="J.R.R. Tolkien",
-			 genre="Fantasy",
-			 description="A book about fantasy.",
-			 price="$15.99",
-			 bookstore=bookstore1)
-session.add(book4)
-session.commit()
+user_json = json.loads(
+    open('users.json', 'r').read())
+bookstore_json = json.loads(
+    open('bookstores.json', 'r').read())
+book_json = json.loads(
+    open('books.json', 'r').read())
 
 
-bookstore2 = Bookstore(user_id=1,
-				   name="Dead Fish")
-session.add(bookstore2)
-session.commit()
+for i in user_json['Users']:
+    user_input = User(
+        id=i['id'],
+        name=str(i['name']),
+        email=str(i['email']),
+        picture=str(i['picture'])
+        )
+    session.add(user_input)
 
-book1 = Book(title="Sherlock Holmes: Selected Stories",
-			 author="Arthur Conan Doyle",
-			 genre="Mystery",
-			 description="A Sherlock Holmes mystery.",
-			 price="$13.39",
-			 bookstore=bookstore2)
-session.add(book1)
-session.commit()
 
-book2 = Book(title="The Murder of Roger Ackroyd",
-			 author="Agatha Christie",
-			 genre="Mystery",
-			 description="A Hercule Poirot mystery.",
-			 price="$13.39",
-			 bookstore=bookstore2)
-session.add(book2)
-session.commit()
+for i in bookstore_json['Bookstores']:
+    bookstore_input = Bookstore(
+        id=i['id'],
+        name=str(i['name']),
+        user_id=i['user_id']
+        )
+    session.add(bookstore_input)
 
-book3 = Book(title="The Murder on the Links",
-			 author="Agatha Christie",
-			 genre="Mystery",
-			 description="A Hercule Poirot mystery.",
-			 price="$12.19",
-			 bookstore=bookstore2)
-session.add(book3)
-session.commit()
 
-bookstore3 = Bookstore(user_id=1,
-				   name="Classic Stars")
-session.add(bookstore3)
-session.commit()
+for i in book_json['Books']:
+    book_input = Book(
+        id=i['id'],
+        title=str(i['title']),
+        author=str(i['author']),
+        genre=str(i['genre']),
+        description=str(i['description']),
+        price=str(i['price']),
+        bookstore_id=i['bookstore_id'],
+        user_id=i['user_id']
+        )
+    session.add(book_input)
 
-book1 = Book(title="The Red and the Black",
-			 author="Stendhal",
-			 genre="French Literature",
-			 description="A classic book.",
-			 price="$23.39",
-			 bookstore=bookstore3)
-session.add(book1)
-session.commit()
 
-book2 = Book(title="Crime and Punishment",
-			 author="Fyodor Dostoyevsky",
-			 genre="Russian Literature",
-			 description="A classic book.",
-			 price="$25.09",
-			 bookstore=bookstore3)
-session.add(book2)
-session.commit()
-
-book3 = Book(title="One Hundred Years of Solitude",
-			 author="Gabriel Garcia Marquez",
-			 genre="Spanish Literature",
-			 description="A classic book.",
-			 price="$24.19",
-			 bookstore=bookstore3)
-session.add(book3)
-session.commit()
-
-bookstore4 = Bookstore(user_id=1,
-				   name="Tupi or not Tupi?")
-session.add(bookstore4)
-session.commit()
-
-book1 = Book(title="The Third Bank of the River and Other Stories",
-			 author="João Guimarães Rosa",
-			 genre="Brazilian Literature",
-			 description="A brazilian book.",
-			 price="$15.39",
-			 bookstore=bookstore4)
-session.add(book1)
-session.commit()
-
-bookstore5 = Bookstore(user_id=1,
-				   name="Updated!")
-session.add(bookstore5)
-session.commit()
-
-book1 = Book(title="Atonement",
-			 author="Ian McEwan",
-			 genre="British Literature",
-			 description="A novel.",
-			 price="$16.89",
-			 bookstore=bookstore5)
-session.add(book1)
-session.commit()
+try:
+    session.commit()
+except Exception:
+    session.rollback()
+    raise
+finally:
+    session.close()
 
 
 print("added books to bookstores!")
-
