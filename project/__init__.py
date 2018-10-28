6ca4579660e519a1b9c8ed5e5757aaf82b58b72c
daniel sc <su7cheng@gmail.com>
2018-10-28 15:49:35 -0300

    Add auth_facebook blueprint.

--- project/__init__.py
+++ project/__init__.py
@@ -5,6 +5,7 @@ from .views.bookstore import bookstore
 from .views.book import book
 from .views.auth_login import auth_login
 from .views.auth_google import auth_google
+from .views.auth_facebook import auth_facebook
 
 
 
@@ -14,6 +15,7 @@ app.register_blueprint(bookstore)
 app.register_blueprint(book)
 app.register_blueprint(auth_login)
 app.register_blueprint(auth_google)
+app.register_blueprint(auth_facebook)
 
 csrf = SeaSurf(app)
 csrf._csrf_disable = False
