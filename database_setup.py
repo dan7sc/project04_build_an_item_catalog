a69e8b48d45f87b143706350e9544b122c806113
daniel sc <su7cheng@gmail.com>
2018-10-28 11:57:18 -0300

    Format database_setup.py with the Python PEP 8 style.

--- database_setup.py
+++ database_setup.py
@@ -5,57 +5,63 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class User(Base):
-	__tablename__ = 'user'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(150), nullable=False)
-	email = Column(String(150), nullable=False)
-	picture = Column(String(250))
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(150), nullable=False)
+    email = Column(String(150), nullable=False)
+    picture = Column(String(250))
 
 
 class Bookstore(Base):
-	__tablename__ = 'bookstore'
+    __tablename__ = 'bookstore'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(150), nullable=False)
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(150), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-	@property
-	def serialize(self):
-		return {
-			'id'			: self.id,
-			'name'			: self.name,
-			'user_id'		: self.user_id,
-		}
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'user_id': self.user_id,
+        }
+
 
 class Book(Base):
-	__tablename__ = 'book'
+    __tablename__ = 'book'
 
-	id = Column(Integer, primary_key=True)
-	title = Column(String(100), nullable=False)
-	author = Column(String(100), nullable=False)
-	genre = Column(String(100))
-	description = Column(String(300))
-	price = Column(String(10))
-	bookstore_id = Column(Integer, ForeignKey('bookstore.id'))
-	bookstore = relationship(Bookstore, backref=backref('bookstore', cascade="all, delete-orphan"))
-	user_id = Column(Integer, ForeignKey('user.id'))
-	user = relationship(User)
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    author = Column(String(100), nullable=False)
+    genre = Column(String(100))
+    description = Column(String(300))
+    price = Column(String(10))
+    bookstore_id = Column(Integer, ForeignKey('bookstore.id'))
+    bookstore = relationship(Bookstore, backref=backref(
+        'bookstore',
+        cascade="all, delete-orphan"
+        ))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-	@property
-	def serialize(self):
-		return {
-			'id'			: self.id,
-			'title'			: self.title,
-			'author'		: self.author,
-			'genre'			: self.genre,
-			'description'	: self.description,
-			'price'			: self.price,
-			'bookstore_id'	: self.bookstore_id,
-			'user_id'		: self.user_id
-		}
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'author': self.author,
+            'genre': self.genre,
+            'description': self.description,
+            'price': self.price,
+            'bookstore_id': self.bookstore_id,
+            'user_id': self.user_id
+        }
+
 
 engine = create_engine('sqlite:///virtualbookstores.db')
 
