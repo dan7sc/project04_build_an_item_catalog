45649d647d15d50520706ffc52563556928ecd67
daniel sc <su7cheng@gmail.com>
2018-10-28 16:01:03 -0300

    Add auth_logout blueprint.

--- project/__init__.py
+++ project/__init__.py
@@ -4,9 +4,9 @@ from flask_seasurf import SeaSurf
 from .views.bookstore import bookstore
 from .views.book import book
 from .views.auth_login import auth_login
+from .views.auth_logout import auth_logout
 from .views.auth_google import auth_google
 from .views.auth_facebook import auth_facebook
-
 
 
 app = Flask(__name__)
@@ -14,6 +14,7 @@ app = Flask(__name__)
 app.register_blueprint(bookstore)
 app.register_blueprint(book)
 app.register_blueprint(auth_login)
+app.register_blueprint(auth_logout)
 app.register_blueprint(auth_google)
 app.register_blueprint(auth_facebook)
 
---
+++ project/views/auth_logout.py
@@ -0,0 +1,27 @@
+from flask import Flask, redirect, url_for, flash
+from flask import session as login_session
+
+from flask import Blueprint
+
+from project.views import auth_google
+from project.views import auth_facebook
+
+
+auth_logout = Blueprint('auth_logout', __name__,
+                        template_folder='templates',
+                        static_folder='static')
+
+
+@auth_logout.route('/disconnect')
+def disconnect():
+    if 'provider' in login_session:
+        if login_session['provider'] == 'google':
+            auth_google.gdisconnect()
+        if login_session['provider'] == 'facebook':
+            auth_facebook.fbdisconnect()
+        del login_session['provider']
+        flash("You have successfully been logged out.")
+        return redirect(url_for('bookstore.showBookstores'))
+    else:
+        flash("You were not logged in.")
+        return redirect(url_for('bookstore.showBookstores'))
