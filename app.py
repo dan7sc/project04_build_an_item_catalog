fcb1e51c36d342c9f30e24b21af324c3d5d2cc9c
daniel sc <su7cheng@gmail.com>
2019-04-12 17:56:51 -0300

    Fix issue: google login button appears again.

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ from project import app
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='localhost', port=8000)
